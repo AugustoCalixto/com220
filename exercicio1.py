ff927ca02f4a8b1235ba44e21a58f91cec632d77
AugustoCalixto <49207917+AugustoCalixto@users.noreply.github.com>
2020-03-11 21:51:21 -0300

    Exercicios 1 e 2 - leitura de string e inteiros

---
+++ exercicio1.py
@@ -0,0 +1,5 @@
+# exercicio 1
+str1 = input("primeira string: ")
+str2 = input("segunda string: ")
+
+print(str2 in str1)
---
+++ exercicio2.py
@@ -0,0 +1,8 @@
+# exercicio 2
+from random import randrange
+
+a = int(input("Digite um numero: "))
+b = int(input("Digite outro numero: "))
+
+
+print(randrange(a,b))
